8161c4dbc6182b8fb02a30b6a4abf377e4d4b551
matine <matine.chabrier@gmail.com>
2018-04-12 20:44:17 +0100

    styled the main nav

--- src/js/components/Nav.js
+++ src/js/components/Nav.js
@@ -9,8 +9,8 @@ class Nav extends Component {
      */
     render() {
         return (
-            <nav>
-                <ul className="list--no-styles list--inline remove-link-styles">
+            <nav className="nav">
+                <ul className="list--no-styles list--inline remove-link-styles font-uppercase font-weight-bold font-size-sm">
                     <li><NavItem to="/about">About</NavItem></li>
                     <li><NavItem to="/work">Work</NavItem></li>
                     <li><NavItem to="/links">Links</NavItem></li>
