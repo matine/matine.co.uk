31bfcb23ccc0bda9dbaa98d81d7aeafaaa8250b3
matine <matine.chabrier@gmail.com>
2018-09-19 15:38:17 +0100

    removed banner old styles

--- src/components/ui/components/Banner.js
+++ src/components/ui/components/Banner.js
@@ -1,19 +1,9 @@
 import styled from 'styled-components';
 import Image from '../objects/Image';
-import { breakpoints } from '../../ui/theme';
 
 const Banner = styled(Image)`
     width: 100%;
     object-fit: cover;
-    position: relative;
-
-    // @media (min-width: ${breakpoints[2]}) {
-    //     position: absolute;
-    //     top: 80px;
-    //     left: 15px;
-    //     z-index: 1;
-    //     width: calc(100% - 30px);
-    // }
 `
 
 export default Banner;
