15f77716f1573136fedd64f2da8a7fa8185d98e2
matine <matine.chabrier@gmail.com>
2018-11-07 01:10:23 +0000

    fixed untracked files

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('/Users/matinechabrier/Sites/matine.co.uk/node_modules/gatsby-plugin-offline/gatsby-browser'),
-      options: {"plugins":[]},
-    },{
-      plugin: require('/Users/matinechabrier/Sites/matine.co.uk/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
