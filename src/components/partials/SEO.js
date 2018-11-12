4e3067a543436c2b874db7c30b2ff8362e6b8399
matine <matine.chabrier@gmail.com>
2018-11-12 21:13:50 +0000

    relinked config to SEO partial

--- src/components/partials/SEO.js
+++ src/components/partials/SEO.js
@@ -2,7 +2,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-import config from '../../../../config/website'
+import config from '../../../config/website'
 
 const SEO = props => {
     const {
